81ab702f8ea29455a7f2b67a331f0283dc4afaf9
Sergey <markrut23@mail.ru>
2024-05-11 19:05:26 +0400

    Alias

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': '/src/app',
+      '@components': '/src/Components',
+      '@pages': '/src/pages',
+      '@shared': '/src/shared',
+      '@widgets': '/src/widgets',
+    }
+  }
 })
